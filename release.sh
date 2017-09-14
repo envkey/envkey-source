7b23d020f6035108f8b7bcecf7a081acc11f6847
Dane Schneider <dane.schneider@gmail.com>
2017-09-14 16:41:09 -0400

    update release script

--- release.sh
+++ release.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-function evil_git_dirty {
-  [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]] && echo "*"
-}
-
-if [ evil_git_dirty == "*" ]; then
-  echo "git index dirty. aborting."
-  exit 1
-fi
-
 version=$(cat version.txt)
 sed -i '' "s/VERSION/${version}/" version/version.go
 git add .
