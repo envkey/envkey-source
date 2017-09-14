c5d8ad308c79530c899c7a988088bf30cd68f85c
Dane Schneider <dane.schneider@gmail.com>
2017-09-14 04:41:23 -0400

    update release script

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 version=$(cat version.txt)
-sed -i 's/VERSION/$version/' version/version.go
+sed -i '' "s/VERSION/${version}/" version/version.go
 git tag "v${version}"
 export GITHUB_TOKEN=$(cat .ghtoken)
 goreleaser --rm-dist
-sed -i 's/$version/VERSION/' version/version.go
+sed -i '' "s/${version}/VERSION/" version/version.go
 git add .
 git commit -am "Built and released ${version}. Updated dist/"
 git push
