9f2f7ec1d1eb4426facf44abe426a7a17077c9e6
Dane Schneider <dane.schneider@gmail.com>
2017-09-14 02:09:15 -0400

    update install/release

--- release.sh
+++ release.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-git tag v$(cat version.txt)
+version=$(cat version.txt)
+git tag "v${version}"
 export GITHUB_TOKEN=$(cat .ghtoken)
 goreleaser --rm-dist
+git add .
+git commit -am "Built and released ${version}. Updated dist/"
+git push
