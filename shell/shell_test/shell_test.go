9e23c779728be6ce4b333eaa2af6a672d0fa5575
Dan Maas <dmaas@maasdigital.com>
2018-03-06 06:21:16 -0600

    add --pam-compatible option
    This changes the output format to be compatible with /etc/environment on Linux.

--- shell/shell_test/shell_test.go
+++ shell/shell_test/shell_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/envkey/envkey-fetch/fetch"
 	"github.com/envkey/envkey-source/shell"
+	"github.com/envkey/envkey-source/version"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,10 +14,15 @@ const INVALID_ENVKEY = "Emzt4BE7C23QtsC7gb1z-3NvfNiG1Boy6XH2oinvalid-env-staging
 
 func TestSource(t *testing.T) {
 	// Test valid
-	validRes := shell.Source(VALID_ENVKEY, true, fetch.FetchOptions{false, ""})
+	validRes := shell.Source(VALID_ENVKEY, true, fetch.FetchOptions{false, "", "envkey-source", version.Version}, false)
 	assert.Equal(t, "export 'TEST'='it' 'TEST_2'='works!' 'TEST_INJECTION'=''\"'\"'$(uname)' 'TEST_SINGLE_QUOTES'='this'\"'\"' is ok' 'TEST_SPACES'='it does work!'", validRes)
 
+	// Test --pam-compatible
+	validRes2 := shell.Source(VALID_ENVKEY, true, fetch.FetchOptions{false, "", "envkey-source", version.Version}, true)
+	assert.Equal(t, "export TEST='it'\nexport TEST_2='works!'\nexport TEST_INJECTION=''$(uname)'\nexport TEST_SINGLE_QUOTES='this' is ok'\nexport TEST_SPACES='it does work!'", validRes2)
+
+
 	// Test invalid
-	invalidRes := shell.Source(INVALID_ENVKEY, true, fetch.FetchOptions{false, ""})
+	invalidRes := shell.Source(INVALID_ENVKEY, true, fetch.FetchOptions{false, "", "envkey-source", version.Version}, false)
 	assert.Equal(t, "echo 'error: ENVKEY invalid'; false", invalidRes)
 }
