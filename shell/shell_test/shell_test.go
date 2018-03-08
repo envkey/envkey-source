42bb5705892b53ba60b9908c94dfc0102699906f
Dane Schneider <dane.schneider@gmail.com>
2018-03-07 20:45:18 -0800

    fix test

--- shell/shell_test/shell_test.go
+++ shell/shell_test/shell_test.go
@@ -5,18 +5,19 @@ import (
 
 	"github.com/envkey/envkey-fetch/fetch"
 	"github.com/envkey/envkey-source/shell"
+	"github.com/envkey/envkey-source/version"
 	"github.com/stretchr/testify/assert"
 )
 
-const VALID_ENVKEY = "Emzt4BE7C23QtsC7gb1z-3NvfNiG1Boy6XH2o-env-staging.envkey.com"
+const VALID_ENVKEY = "CeUWMgVdYnyx7TBCmA7t-28YX8LzjHpJuMbFt-env-staging.envkey.com"
 const INVALID_ENVKEY = "Emzt4BE7C23QtsC7gb1z-3NvfNiG1Boy6XH2oinvalid-env-staging.envkey.com"
 
 func TestSource(t *testing.T) {
 	// Test valid
-	validRes := shell.Source(VALID_ENVKEY, true, fetch.FetchOptions{false, ""})
+	validRes := shell.Source(VALID_ENVKEY, true, fetch.FetchOptions{false, "", "envkey-source", version.Version})
 	assert.Equal(t, "export 'TEST'='it' 'TEST_2'='works!' 'TEST_INJECTION'=''\"'\"'$(uname)' 'TEST_SINGLE_QUOTES'='this'\"'\"' is ok' 'TEST_SPACES'='it does work!'", validRes)
 
 	// Test invalid
-	invalidRes := shell.Source(INVALID_ENVKEY, true, fetch.FetchOptions{false, ""})
+	invalidRes := shell.Source(INVALID_ENVKEY, true, fetch.FetchOptions{false, "", "envkey-source", version.Version})
 	assert.Equal(t, "echo 'error: ENVKEY invalid'; false", invalidRes)
 }
