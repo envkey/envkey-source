326a37873ecba32d9f269bb156cc7980f99d59d8
Dane Schneider <dane.schneider@gmail.com>
2018-09-14 11:55:07 -0700

    fix test live ENVKEY

--- shell/shell_test/shell_test.go
+++ shell/shell_test/shell_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const VALID_ENVKEY = "CeUWMgVdYnyx7TBCmA7t-28YX8LzjHpJuMbFt-env-staging.envkey.com"
+const VALID_ENVKEY = "wYv78UmHsfEu6jSqMZrU-3w1kwyF35nRYwsAJ-env-staging.envkey.com"
 const INVALID_ENVKEY = "Emzt4BE7C23QtsC7gb1z-3NvfNiG1Boy6XH2oinvalid-env-staging.envkey.com"
 
 func TestSource(t *testing.T) {
